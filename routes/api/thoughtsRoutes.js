b783262c9e5ca0c2486d37c0f9740edae0b2ac0f
Francisco J Gutierrez Rodarte <Francisco.GutierrezRodarte@du.edu>
2023-04-11 19:46:07 -0700

    updated routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -9,26 +9,6 @@ const {
   removeReaction,
 } = require("../../controllers/thoughtsController.js");
 
-// * `GET` to get all thoughts
-
-// * `GET` to get a single thought by its `_id`
-
-// * `POST` to create a new thought (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
-
-// ```json
-// // example data
-// {
-//   "thoughtText": "Here's a cool thought...",
-//   "username": "lernantino",
-//   "userId": "5edff358a0fcb779aa7b118b"
-// }
-// ```
-
-// * `PUT` to update a thought by its `_id`
-
-// * `DELETE` to remove a thought by its `_id`
-
-// ---
 // /api/thoughts
 router.route("/").get(getThoughts).post(createThought);
 
@@ -38,12 +18,6 @@ router
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
-
-// **`/api/thoughts/:thoughtId/reactions`**
-
-// * `POST` to create a reaction stored in a single thought's `reactions` array field
-
-// * `DELETE` to pull and remove a reaction by the reaction's `reactionId` value
 
 // /api/thoughts/:thoughtId/reactions/
 router.route("/:thoughtId/reactions").post(addReaction);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,12 +15,6 @@ router.route("/").get(getUsers).post(createUser);
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// **`/api/users/:userId/friends/:friendId`**
-
-// * `POST` to add a new friend to a user's friend list
-
-// * `DELETE` to remove a friend from a user's friend list
-
 // /api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
